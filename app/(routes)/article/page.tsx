92dd7c41635aed1ed6d1dffa57ec96aff29dfd01
ruby.chen <ruby.chen@cheil.com.tw>
2024-11-22 17:30:52 +0800

    feat: modified scroll animation

--- app/(routes)/article/page.tsx
+++ app/(routes)/article/page.tsx
@@ -1,32 +1,33 @@
 "use client";
-// import { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { Navbar } from "@/app/(ui)/shareComponent/Navbar";
 import { Footer } from "@/app/(ui)/shareComponent/Footer";
-// import { motion, useScroll } from "motion/react";
-// import Image from "next/image";
+import { motion, useScroll, useTransform } from "motion/react";
 
 export default function Page() {
-//  const items = useRef<(HTMLLIElement | null)[]>([]);
-//  const sectionRef = useRef<HTMLElement | null>(null);
-//  const progressRef = useRef<HTMLDivElement | null>(null);
-//  const { scrollYProgress } = useScroll({ target: sectionRef });
-// useEffect(() => {
-//   // 進行滾動條動畫
-//   const interval = setInterval(() => {
-//     if (progressRef.current) {
-//       progressRef.current.style.transform = `scaleX(${scrollYProgress.get()})`;
-//     }
-//   }, 10);
+  // const items = useRef<(HTMLLIElement | null)[]>([]);
+  const sectionRef = useRef<HTMLElement | null>(null);
+  const progressRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({ target: sectionRef });
+  const xPosition = useTransform(scrollYProgress, [0, 1], [0, 1600]);
+  useEffect(() => {
+    // 進行滾動條動畫
+    const interval = setInterval(() => {
+      if (progressRef.current) {
+        progressRef.current.style.transform = `scaleX(${scrollYProgress.get()})`;
+      }
+    }, 10);
 
-//   return () => clearInterval(interval);
-// }, [scrollYProgress]);
+    return () => clearInterval(interval);
+  }, [scrollYProgress]);
 
   return (
     <div className="grid grid-rows-[8rem_1fr_6rem] min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <Navbar />
       <main className="flex flex-col gap-4 row-start-2  justify-center items-center">
         {/* test start */}
-        {/* <div className="App bg-black text-white min-h-screen flex flex-col">
+
+        <div className="App bg-black text-white min-h-screen flex flex-col">
           <header className="flex justify-between items-center p-4">
             <h1 className="text-white text-sm flex items-center space-x-2">
               <a href="https://motion.dev" target="_blank" rel="noopener noreferrer">
@@ -52,13 +53,44 @@ export default function Page() {
                 <h2 className="text-4xl text-white">Lines of London</h2>
               </header>
               <section ref={sectionRef} className="relative h-[500vh]">
+                <motion.div
+                  style={{
+                    width: "100%",
+                    height: "100px",
+                    backgroundColor: "blue",
+                    position: "absolute",
+                    top: "100px",
+                    x: xPosition, // Set the dynamic Y position based on scroll
+                  }}>
+                  Scroll Me
+                </motion.div>
                 <ul className="flex sticky top-0">
-                  {Array.from({ length: 5 }, (_, i) => (
+                  <motion.li initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 1 * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
+                    <div className="w-[300px] h-[400px] bg-slate-100" />
+                    <h3 className="text-5xl text-red-500">#1</h3>
+                  </motion.li>
+                  <motion.li initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 2 * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
+                    <div className="w-[300px] h-[400px] bg-slate-200" />
+                    <h3 className="text-5xl text-red-500">#2</h3>
+                  </motion.li>
+                  <motion.li initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 3 * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
+                    <div className="w-[300px] h-[400px] bg-slate-300" />
+                    <h3 className="text-5xl text-red-500">#3</h3>
+                  </motion.li>
+                  <motion.li initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 4 * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
+                    <div className="w-[300px] h-[400px] bg-slate-400" />
+                    <h3 className="text-5xl text-red-500">#4</h3>
+                  </motion.li>
+                  <motion.li initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 5 * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
+                    <div className="w-[300px] h-[400px] bg-slate-500" />
+                    <h3 className="text-5xl text-red-500">#5</h3>
+                  </motion.li>
+                  {/* {Array.from({ length: 5 }, (_, i) => (
                     <motion.li key={i} ref={(el) => (items.current[i] = el) } initial={{ opacity: 0, x: 200 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: i * 0.2, duration: 0.8 }} className="flex w-full h-screen items-center justify-center flex-col">
-                      <Image src={`/${i + 1}.jpg`} alt={`Image ${i + 1}`} className="w-[300px] h-[400px]" />
+                      <div className={`w-[300px] h-[400px] bg-slate-${}00`} />
                       <h3 className="text-5xl text-red-500">#{i + 1}</h3>
                     </motion.li>
-                  ))}
+                  ))} */}
                 </ul>
               </section>
               <footer className="bg-black text-center py-4">
@@ -72,7 +104,7 @@ export default function Page() {
             </article>
             <div className="fixed left-0 right-0 h-1 bg-accent transform scale-x-0" ref={progressRef}></div>
           </main>
-        </div>   */}
+        </div>
         {/* test end */}
       </main>
       <Footer />
