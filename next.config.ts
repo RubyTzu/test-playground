8b44453dc442d831d0ded40c8573460237a5bcb2
ruby.chen <ruby.chen@cheil.com.tw>
2025-03-10 10:27:01 +0800

    feat: modified next.config.ts for errors

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withImages = require("next-images");
-module.exports = withImages({
-  fileExtensions: ["jpg", "jpeg", "png", "gif"],
-});
-
 export default nextConfig;
