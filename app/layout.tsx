f8c4b960fe722054141d136c23d059792330ed79
ruby.chen <rruby6120tw@gmail.com>
2025-03-27 14:16:06 +0800

    feat:modified meta openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,44 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Ruby Yi Tzu Chen",
-  description: "I am an explorer passionate about learning new skills.",
-
+  title: "Next JS Test",
+  description: "A place for me to practice NextJS",
+  metadataBase: new URL("https://nextjs-dasboard-woad.vercel.app"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "zh-TW": "/zh-TW",
+    },
+  },
+  openGraph: {
+    title: "Ruby Yi Tzu Chen",
+    description: "An explorer passionate about learning new skills.",
+    url: "https://rrubyitzuchen.vercel.app",
+    siteName: "Ruby Yi Tzu Chen",
+    images: [
+      {
+        url: "/images/openGraph/1200x630.png",
+        width: 1200,
+        height: 630,
+        alt: "A descriptive alt text for the image",
+      },
+      {
+        url: "/images/openGraph/600x315.png",
+        width: 600,
+        height: 315,
+        alt: "A descriptive alt text for the image",
+      },
+      {
+        url: "/images/openGraph/200x200.png",
+        width: 200,
+        height: 200,
+        alt: "A descriptive alt text for the image",
+      },
+    ],
+    locale: "zh_TW",
+    type: "website",
+  },
 };
 
 export default async function RootLayout({
@@ -32,6 +67,18 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="Ruby Yi Tzu Chen" />
+        <meta property="og:description" content="An explorer passionate about learning new skills." />
+        <meta property="og:url" content="https://rrubyitzuchen.vercel.app" />
+        <meta property="og:site_name" content="Ruby Yi Tzu Chen" />
+        <meta property="og:locale" content="zh_TW" />
+        <meta property="og:image:url" content="/images/openGraph/1200_630.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="My custom alt" />
+        <meta property="og:type" content="website" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutrals-0 text-neutrals-300 selection:text-primary-500 selection:bg-primary-200`}>
         <Providers>
           <div className="grid grid-rows-[8rem_1fr_6rem] min-h-screen font-[family-name:var(--font-geist-sans)]">
