7a7817db5fdd2f2a1e1f72d267b47115d3548de9
Ruby <rruby6120tw@gmail.com>
2025-05-17 17:56:40 +0800

    feat: modified opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,17 @@ import { BackToTopButton } from "./(ui)/shareComponent/BackToTopBtn";
 import { MainContainer } from "./(ui)/shareComponent/MainContainer";
 import { Providers } from "./(data)/Provider";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
 // export async function generateMetadata(): Promise<Metadata> {
 
 //   return {
@@ -53,31 +64,32 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Ruby Yi Tzu Chen",
     description: "An explorer passionate about learning new skills.",
-    url: "https://www.rrubyitzuchen.com",
+    url: "https://www.rrubyitzuchen.com/",
     siteName: "Ruby Yi Tzu Chen",
     images: [
       {
-        url: "https://www.rrubyitzuchen.com/images/openGraph/1200x630.png",
+        url: "/images/openGraph/1200x630.png",
         width: 1200,
         height: 630,
-        alt: "Large preview",
+        alt: "A descriptive alt text for the image",
+      },
+      {
+        url: "/images/openGraph/600x315.png",
+        width: 600,
+        height: 315,
+        alt: "A descriptive alt text for the image",
+      },
+      {
+        url: "/images/openGraph/200x200.png",
+        width: 200,
+        height: 200,
+        alt: "A descriptive alt text for the image",
       },
     ],
     locale: "zh_TW",
     type: "website",
   },
 };
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default async function RootLayout({
   children,
@@ -89,6 +101,16 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:title" content="Ruby Yi Tzu Chen" />
+        <meta property="og:description" content="An explorer passionate about learning new skills." />
+        <meta property="og:url" content="https://www.rrubyitzuchen.com/" />
+        <meta property="og:site_name" content="Ruby Yi Tzu Chen" />
+        <meta property="og:locale" content="zh_TW" />
+        <meta property="og:image:url" content="/images/openGraph/1200x630.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="My custom alt" />
+        <meta property="og:type" content="website" />
         {GA_ID && (
           <>
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
@@ -104,16 +126,6 @@ export default async function RootLayout({
             />
           </>
         )}
-        <meta property="og:title" content="Ruby Yi Tzu Chen" />
-        <meta property="og:description" content="An explorer passionate about learning new skills." />
-        <meta property="og:url" content="https://www.rrubyitzuchen.com" />
-        <meta property="og:site_name" content="Ruby Yi Tzu Chen" />
-        <meta property="og:locale" content="zh_TW" />
-        <meta property="og:image:url" content="/images/openGraph/1200x630.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="My custom alt" />
-        <meta property="og:type" content="website" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutrals-0 text-neutrals-300 selection:text-primary-500 selection:bg-primary-200 2xl:text-lg`}>
         <Providers>
