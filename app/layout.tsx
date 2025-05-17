fed8a094eaf381e16157bf16d3392db79d499b73
Ruby <rruby6120tw@gmail.com>
2025-05-17 16:46:10 +0800

    feat:modified opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,38 @@ import { MainContainer } from "./(ui)/shareComponent/MainContainer";
 import { Providers } from "./(data)/Provider";
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Ruby Yi Tzu Chen",
+    description: "An explorer passionate about learning new skills.",
+    metadataBase: new URL(siteUrl),
+    alternates: {
+      canonical: "/",
+      languages: {
+        "en-US": "/en-US",
+        "zh-TW": "/zh-TW",
+      },
+    },
+    openGraph: {
+      title: "Ruby Yi Tzu Chen",
+      description: "An explorer passionate about learning new skills.",
+      url: siteUrl,
+      siteName: "Ruby Yi Tzu Chen",
+      images: [
+        {
+          url: `${siteUrl}/images/openGraph/1200x630.png`,
+          width: 1200,
+          height: 630,
+          alt: "A descriptive alt text for the image",
+        },
+      ],
+      locale: "zh_TW",
+      type: "website",
+    },
+  };
+}
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,66 +52,14 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Ruby Yi Tzu Chen",
-  description: "An explorer passionate about learning new skills.",
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000"),
-  alternates: {
-    canonical: "/",
-    languages: {
-      "en-US": "/en-US",
-      "zh-TW": "/zh-TW",
-    },
-  },
-  openGraph: {
-    title: "Ruby Yi Tzu Chen",
-    description: "An explorer passionate about learning new skills.",
-    url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
-    siteName: "Ruby Yi Tzu Chen",
-    images: [
-      {
-        url: "/images/openGraph/1200x630.png",
-        width: 1200,
-        height: 630,
-        alt: "A descriptive alt text for the image",
-      },
-      {
-        url: "/images/openGraph/600x315.png",
-        width: 600,
-        height: 315,
-        alt: "A descriptive alt text for the image",
-      },
-      {
-        url: "/images/openGraph/200x200.png",
-        width: 200,
-        height: 200,
-        alt: "A descriptive alt text for the image",
-      },
-    ],
-    locale: "zh_TW",
-    type: "website",
-  },
-};
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
       <head>
-        <meta property="og:title" content="Ruby Yi Tzu Chen" />
-        <meta property="og:description" content="An explorer passionate about learning new skills." />
-        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}`} />
-        <meta property="og:site_name" content="Ruby Yi Tzu Chen" />
-        <meta property="og:locale" content="zh_TW" />
-        <meta property="og:image:url" content="/images/openGraph/1200x630.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="My custom alt" />
-        <meta property="og:type" content="website" />
         {GA_ID && (
           <>
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
